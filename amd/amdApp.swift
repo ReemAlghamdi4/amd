af4c668efe55db3d47d8a1f6281740b407c96174
Reem <ReemAlghamdi4>
2025-11-28 12:04:00 +0300

    Initial Commit

---
+++ amd/amdApp.swift
@@ -0,0 +1,17 @@
+//
+//  amdApp.swift
+//  amd
+//
+//  Created by Reem alghamdi on 07/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct amdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ amdTests/amdTests.swift
@@ -0,0 +1,16 @@
+//
+//  amdTests.swift
+//  amdTests
+//
+//  Created by Reem alghamdi on 07/06/1447 AH.
+//
+
+import Testing
+
+struct amdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
